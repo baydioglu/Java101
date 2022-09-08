6cea840f98d1aa8b3ee93586440acfb1a299e7c5
Uğur <40892114+baydioglu@users.noreply.github.com>
2022-09-08 19:23:56 +0300

    Sayıları Büyükten Küçüğe Sıralama
    Sayıları Büyükten Küçüğe Sıralama pratiği için oluşturulmuş java dosyasıdır.

---
+++ Siralama.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Siralama {
+    public static void main(String[] args) {
+
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("1. sayıyı giriniz: ");
+            a = input.nextInt();
+
+            System.out.print("2. sayıyı giriniz: ");
+            b = input.nextInt();
+
+            System.out.print("3. sayıyı giriniz: ");
+            c = input.nextInt();
+
+
+            if ((a == b) || (b == c) || (a == c)) {
+                System.out.println("Eşit sayılar kullandınız, lütfen yeniden giriş yapınız. ");
+            } else  {
+                if ((a < b) && (a < c)) {
+                    if (b < c) {
+                        System.out.println(a + "<" + b + "<" + c);
+
+                    } else
+                        System.out.println(a + "<" + c + "<" + b);
+                } else if ((b < a) && (b < c)) {
+                    if (a < c) {
+                        System.out.println(b + "<" + a + "<" + c);
+
+                    } else
+                        System.out.println(b + "<" + c + "<" + a);
+
+                } else {
+                    if (b < a) {
+                        System.out.println(c + "<" + b + "<" + a);
+
+                    } else
+                        System.out.println(c + "<" + a + "<" + b);
+                }
+            }
+        }
+
+    }
+}
